40f7361947e50057696b323fcb70f64ce58b282c
Kwanhathai <kwanhathai.c@azay.co.th>
2019-06-05 00:48:05 +0700

    remove
    remove

--- config.php
+++ config.php
@@ -12,7 +12,5 @@ mysql_query("SET NAMES UTF8");
 mysql_query("SET character_set_results=utf8");
 mysql_query("SET character_set_client=utf8");
 mysql_query("SET character_set_connection=utf8");
-//
-//tets
-//relase4.1
+
 ?>
